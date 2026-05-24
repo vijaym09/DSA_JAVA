55e12365ea53ba2562efedd0757c9287e182dd4e
vijaym09 <vijay.rtm1@gmail.com>
2026-05-24 18:23:17 +0000

    This is my today's Code

--- Methods/src/TwoTable.java
+++ Methods/src/TwoTable.java
@@ -1,13 +1,13 @@
-public class TwoTable {
+// public class TwoTable {
 
-    void printTwoTable()
-    {
-        for(int i=1; i<=10;i++){
-            int ans=2*i;
-            System.out.println(ans);
-        }
-    }
-    static void main() {
+//     void printTwoTable()
+//     {
+//         for(int i=1; i<=10;i++){
+//             int ans=2*i;
+//             System.out.println(ans);
+//         }
+//     }
+//     static void main() {
 
-    }
-}
+//     }
+// }
---
+++ Strings/StringToUpperCase.java
@@ -0,0 +1,11 @@
+//Convert String to uppercase with the using method
+
+public class StringToUpperCase {
+    public static void main(String[] args) {
+        String str="love";
+       System.out.println(str.toUpperCase());
+
+
+    }
+
+}
