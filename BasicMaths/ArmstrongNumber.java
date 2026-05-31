5c4e659f310213c34a8fca00c7f37adf6a10e5d1
vijaym09 <vijay.rtm1@gmail.com>
2026-05-31 16:12:37 +0000

    Code

---
+++ BasicMaths/ArmstrongNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int originalNum = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += (int) Math.pow(digit, digits);
+            num = num / 10;
+        }
+
+        if (sum == originalNum) {
+            System.out.println(originalNum + " is an Armstrong Number");
+        } else {
+            System.out.println(originalNum + " is not an Armstrong Number");
+        }
+
+        sc.close();
+    }
+}
