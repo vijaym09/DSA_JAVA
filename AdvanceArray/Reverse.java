afb01f9e8fdd4185cfc7b635d864d502cb72d660
vijaym09 <vijay.rtm1@gmail.com>
2026-07-03 13:41:32 +0000

    Code

---
+++ AdvanceArray/Reverse.java
@@ -0,0 +1,9 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,8,6,5};
+        for(int i=arr.length-1;i>0;i--){
+            System.err.println(arr[i]);
+        }
+    }
+        
+}
